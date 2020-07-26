9f677dc7beb1dbe0404945eb0e738d2ef599e7e7
Dave Compton <davecompton7@gmail.com>
2020-07-26 16:36:52 -0700

    cleaned up

--- src/Lib.hs
+++ src/Lib.hs
@@ -3,13 +3,10 @@
 
 module Lib where
 
-import Data.Aeson (eitherDecode, FromJSON, Object, parseJSON, withArray, withObject, (.:), (.:?))
-import Data.Aeson.Types (Parser)
-import GHC.Generics (Generic)
-import qualified Data.ByteString.Lazy as BL (ByteString)
-import qualified Data.HashMap.Strict as HashMap (empty, unions)
-import Data.Function ((&))
-import Data.Foldable (toList)
+import Data.Aeson 
+import Data.Aeson.Types 
+import GHC.Generics 
+import Data.Map as DM
 
 data Abc = Abc
   { name :: String
@@ -20,21 +17,14 @@ data Abc = Abc
 instance FromJSON Abc where
   parseJSON =
    withObject "Abc" $ \v -> do
-    extra <- getExtra v
-    Abc <$> v .: "name" <*> extra .:? "this_string_A" <*> extra .:? "this_string_B"
-
-getExtra :: Object -> Parser Object
-getExtra v = do
-  mextra <- v .:? "extra"
-  case mextra of
-    Just vv -> vv & withArray "Abc.extra" (\arr -> do
-      let vallst = toList arr
-      objlst <- traverse (withObject "Abc.extra[..]" pure) vallst
-      return $ HashMap.unions objlst)
-    Nothing -> return HashMap.empty
-
-example :: BL.ByteString
-example = "{\"name\": \"xyz1\", \"extra\": [{\"this_string_A\": \"Hello\"}, {\"this_string_B\": \"World\"}]}"
+    name <- v.: "name"
+    extra0 <- v.: "extra" :: Parser [Map String String]
+    let extra = Prelude.foldr union empty extra0
+        a = DM.lookup "this_string_A" extra
+        b = DM.lookup "this_string_B" extra
+    return $ Abc name a b
 
 test :: Either String Abc
-test = eitherDecode example
+test = eitherDecode exampleJson
+
+exampleJson = "{ \"name\": \"xyz1\", \"extra\": [ { \"this_string_A\": \"Hello\" }, { \"this_string_B\": \"World\" } ] }"
